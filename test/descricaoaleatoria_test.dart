f70413b4cca9d37674e75db30c9b238c116e219b
Rodrigo Guimarães <rguimaraes@ons.org.br>
2022-07-01 23:25:59 -0300

    Adicionar teste unitário

--- test/descricaoaleatoria_test.dart
+++ test/descricaoaleatoria_test.dart
@@ -3,10 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:descricaoaleatoria/descricaoaleatoria.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Verificar se obteve o item da API', () {
+
+    
+  Future<RandomAdvice> future = RandomAdvice.getRandomAdvice();
+  future.then((valor) {
+
+    expect(valor.slip, isNot(equals(null)));
+   
   });
+  expect(future, completes);
+});
+
+    
+
+  
 }
